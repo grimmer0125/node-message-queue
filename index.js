0b4dd09648b2288cb16cb97e12954ed89b9b8dcd
Grimmer <grimmer0125@gmail.com>
2018-04-02 17:05:35 +0800

    add rpc_server.py to test RabbitMQ more.

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const rabbit = require('./rpc_client.js');
-const zero = require('./z-client.js');
+// const zero = require('./z-client.js');
 
 const app = express();
 
-app.get('/test', (req, res) => {
+app.get('/test', async (req, res) => {
   console.log('request to test url:', req.method.toString()); // still 兩次, get, head
 
   if (req.method == 'GET') {
     console.log('start test');
-    // Test RabbitMQ
-    // const data = await rabbit.tryRabbitMQRPCFib(10);
-    // console.log('data:', data);
 
-    Test zeromq
-    console.log('start to test zeromq');
-    const data = await zero.testZMQReqResp();
+    // Test RabbitMQ
+    const data = await rabbit.tryRabbitMQRPCFib(10);
     console.log('data:', data);
-    console.log('end to test zeromq');
+
+    // Test zeromq
+    // console.log('start to test zeromq');
+    // const data = await zero.testZMQReqResp();
+    // console.log('data:', data);
+    // console.log('end to test zeromq');
   }
 
   console.log('end to request to test url');
