cbcf9d831c91dcd285ad5a640271e0841c45acd5
Grimmer <grimmer0125@gmail.com>
2018-04-03 23:23:09 +0800

    add some comments.

--- stop.sh
+++ stop.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # kubectl delete services express-rabbit-pod
+# kubectl delete deployment express-rabbit-pod or delete pod
 
-# kubectl delete deployment express-rabbit-pod
+# kubectl delete -f pod-config.yaml if use kubectl create -f pod-config.yaml
 
 # minikube stop
