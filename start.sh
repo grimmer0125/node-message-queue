10bebd0fb3ce0127ae97123f18d6bb6274543c10
Grimmer <grimmer0125@gmail.com>
2018-04-03 12:30:25 +0800

    add two scripts for k8s.

---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# to build and use the resulted docker image in minikube
+# eval $(minikube docker-env)
+
+# make docker image
+# docker build -t grimmer0125/express-rabbit .
+
+# start docker for testing
+# # docker run -p 3000:3000 -ti grimmer0125/express-rabbit:latest
+
+# start k8s
+# minikube start.
+# kubectl run express-rabbit-pod --image-pull-policy=Never --image=grimmer0125/express-rabbit:latest --port=3000
+
+# into its shell for tesing
+# export POD_NAME=express-rabbit-pod-7469d7b866-qd7ck (deployment name + someid)
+# kubectl exec -ti $POD_NAME bash
+# curl localhost:3000
+
+# add the service: https://kubernetes.io/docs/getting-started-guides/minikube/
+# kubectl expose deployment express-rabbit-pod --type=NodePort  <-should be ok
+# kubectl expose deployment express-rabbit-pod --type=NodePort --port 3000
+# curl $(minikube service express-rabbit-pod --url) e.g. http://192.168.99.100:32560
+# 192.168.99.100 is minikube ip
+# curl $(minikube ip):$NODE_PORT
+# kubectl exec -ti $POD_NAME curl localhost:8080
+
+# https://kubernetes.io/docs/tutorials/kubernetes-basics/expose-interactive/
+# minikube does not support the LoadBalancer option yet)
---
+++ stop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# kubectl delete services express-rabbit-pod
+
+# kubectl delete deployment express-rabbit-pod
+
+# minikube stop
